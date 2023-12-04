8083706f42f06945c61690915c7d2bcd53b606f9
Erik Kratochvil <erik.kratochvil@team.wrike.com>
2023-12-04 07:56:00 +0100

    Day 4 Refactor

--- 2023/src/main/java/com/github/eyrekr/Day4.java
+++ 2023/src/main/java/com/github/eyrekr/Day4.java
@@ -4,7 +4,7 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
+import java.util.Arrays;
 import java.util.Set;
 
 class Day4 {
@@ -20,17 +20,17 @@ class Day4 {
 
     public static void main(String[] args) throws Exception {
         //StringUtils.split(SAMPLE, '\n')
-        final List<String> lines = Files.readAllLines(Path.of("src/main/resources/04.txt"));
+        final Card[] deck = Files.readAllLines(Path.of("src/main/resources/04.txt")).stream()
+                .map(Card::from)
+                .toArray(Card[]::new);
+
         long sum = 0;
-        for (final String line : lines) {
-            final String ticket = StringUtils.substringAfter(line, ":");
-            final String[] numbers = StringUtils.split(ticket, '|');
-            final Set<String> winningNumbers = Set.of(StringUtils.split(numbers[0]));
-            final String[] numbersYouHave = StringUtils.split(numbers[1]);
+        for (int i = 0; i < deck.length; i++) {
+            final Card card = deck[i];
 
             long points = 0;
-            for (final String numberYouHave : numbersYouHave) {
-                if (winningNumbers.contains(numberYouHave)) {
+            for (final String numberYouHave : card.numbersYouHave) {
+                if (card.winningNumbers.contains(numberYouHave)) {
                     if (points == 0) {
                         points = 1;
                     } else {
@@ -43,5 +43,28 @@ class Day4 {
         }
 
         System.out.printf("SUM = %d", sum);
+    }
+
+    static class Card {
+        final Set<String> winningNumbers;
+        final String[] numbersYouHave;
+        final long matchingNumbers;
+
+        int instances = 1;
+
+        Card(final Set<String> winningNumbers, final String[] numbersYouHave) {
+            this.winningNumbers = winningNumbers;
+            this.numbersYouHave = numbersYouHave;
+            this.matchingNumbers = Arrays.stream(numbersYouHave).filter(winningNumbers::contains).count();
+        }
+
+        static Card from(final String line) {
+            final String ticket = StringUtils.substringAfter(line, ":");
+            final String[] numbers = StringUtils.split(ticket, '|');
+            final Set<String> winningNumbers = Set.of(StringUtils.split(numbers[0]));
+            final String[] numbersYouHave = StringUtils.split(numbers[1]);
+
+            return new Card(winningNumbers, numbersYouHave);
+        }
     }
 }
