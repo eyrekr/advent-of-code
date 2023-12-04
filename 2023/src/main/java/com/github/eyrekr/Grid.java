3d0901a19ff7fbd503835b92a87ccdfdb6d55ebc
Erik Kratochvil <erik.kratochvil@team.wrike.com>
2023-12-04 12:04:05 +0100

    Grid work

--- 2023/src/main/java/com/github/eyrekr/Grid.java
+++ 2023/src/main/java/com/github/eyrekr/Grid.java
@@ -6,17 +6,19 @@ import java.nio.file.Path;
 import java.util.Iterator;
 import java.util.List;
 
-record Grid(int m, int n, char[][] data) implements Iterable<Grid.Item> {
+record Grid(int m, int n, char[][] data) implements Iterable<Grid.It> {
+
+    static final char C0 = '\0';
 
     static Grid from(final Path path) {
         try {
-            return from(Files.readAllLines(Path.of("src/main/resources/03.txt")));
+            return Grid.of(Files.readAllLines(path));
         } catch (final IOException e) {
             throw new IllegalStateException(e);
         }
     }
 
-    static Grid from(final List<String> lines) {
+    static Grid of(final List<String> lines) {
         final int n = lines.size();
         final int m = lines.get(0).length();
         final char[][] data = new char[m][n];
@@ -29,8 +31,8 @@ record Grid(int m, int n, char[][] data) implements Iterable<Grid.Item> {
         return new Grid(m, n, data);
     }
 
-    char at(int x, int y, char defaultValue) {
-        return x >= 0 && x < m && y >= 0 && y < n ? data[x][y] : defaultValue;
+    char at(int x, int y) {
+        return x >= 0 && x < m && y >= 0 && y < n ? data[x][y] : C0;
     }
 
     Grid transpose() {
@@ -45,16 +47,16 @@ record Grid(int m, int n, char[][] data) implements Iterable<Grid.Item> {
 
 
     @Override
-    public Iterator<Item> iterator() {
+    public Iterator<It> iterator() {
         return new GridIterator();
     }
 
-    record Item(int x, int y, char ch, int digit, boolean first, boolean last, boolean firstOnLine,
-                boolean lastOnLine) {
+    record It(int x, int y, char ch, char[] neighbours4, char[] neighbours8, int digit,
+              boolean first, boolean last, boolean firstOnLine, boolean lastOnLine) {
     }
 
-    private class GridIterator implements Iterator<Grid.Item> {
-        private int i = -1;
+    private class GridIterator implements Iterator<It> {
+        private int i = 0;
 
         @Override
         public boolean hasNext() {
@@ -62,15 +64,18 @@ record Grid(int m, int n, char[][] data) implements Iterable<Grid.Item> {
         }
 
         @Override
-        public Item next() {
-            i++;
+        public It next() {
             if (i >= m * n) {
                 throw new IllegalStateException();
             }
             final int x = i % m;
             final int y = i / n;
             final char ch = data[x][y];
-            return new Item(x, y, ch, "0123456789".indexOf(ch), i == 0, i == m * n - 1, x == 0, x == m - 1);
+            final char[] ch4 = new char[]{at(x, y - 1), at(x - 1, y), at(x + 1, y), at(x, y + 1)};
+            final char[] ch8 = new char[]{at(x - 1, y - 1), at(x, y - 1), at(x + 1, y - 1), at(x - 1, y), at(x + 1, y), at(x - 1, y + 1), at(x, y + 1), at(x + 1, y + 1)};
+            final It it = new It(x, y, ch, ch4, ch8, "0123456789".indexOf(ch), i == 0, i == m * n - 1, x == 0, x == m - 1);
+            i++;
+            return it;
         }
     }
 }
