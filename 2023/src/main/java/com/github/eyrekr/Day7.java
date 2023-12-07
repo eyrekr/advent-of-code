be7148588db165523ca22c26307146941b558a96
Erik Kratochvil <erik.kratochvil@team.wrike.com>
2023-12-07 09:49:47 +0100

    Day 7 Simplify

--- 2023/src/main/java/com/github/eyrekr/Day7.java
+++ 2023/src/main/java/com/github/eyrekr/Day7.java
@@ -16,31 +16,19 @@ import static java.util.stream.Collectors.groupingBy;
  */
 class Day7 {
 
-    static final String SAMPLE = """
-            32T3K 765
-            T55J5 684
-            KK677 28
-            KTJJT 220
-            QQQJA 483
-            """;
+    static final char JOKER = '*';
 
     public static void main(String[] args) throws Exception {
-        //final String[] lines = SAMPLE.split("\n");
         final String[] lines = Files.readString(Path.of("src/main/resources/07.txt")).split("\n");
-        System.out.printf("%d\n%d\n", star1(lines), star2(lines));
+        System.out.printf("%d\n%d\n",
+                solve(Seq.fromArray(lines).map(line -> Card.from(line, false))),
+                solve(Seq.fromArray(lines).map(line -> Card.from(line, true))));
     }
 
-    static long star1(final String[] lines) {
-        return Seq.fromArray(lines).map(line -> Card.from(line, false))
+    static long solve(final Seq<Card> cards) {
+        return cards
                 .sortedBy(Card::compareTo)
-                .mapWith(Seq.range(0, lines.length), Card::changeOrder)
-                .reduce(0L, (acc, card) -> acc + card.bid * (card.order + 1));
-    }
-
-    static long star2(final String[] lines) {
-        return Seq.fromArray(lines).map(line -> Card.from(line, true))
-                .sortedBy(Card::compareTo)
-                .mapWith(Seq.range(0, lines.length), Card::changeOrder)
+                .mapWith(Seq.range(0, cards.length), Card::changeOrder)
                 .print("\n")
                 .reduce(0L, (acc, card) -> acc + card.bid * (card.order + 1));
     }
@@ -48,33 +36,33 @@ class Day7 {
     enum Type {
         HighCard, OnePair, TwoPair, ThreeOfKind, FullHouse, FourOfKind, FiveOfKind;
 
-        static Type from(final String input, final boolean allowJokers) {
+        static Type from(final String input) {
             final var histogram = input.chars().boxed().collect(groupingBy(identity()));
             final var distinctNumbers = histogram.size();
             final var longestRun = histogram.values().stream().mapToInt(List::size).max().getAsInt();
-            final var jokers = allowJokers ? StringUtils.countMatches(input, 'J') : 0;
+            final var jokers = StringUtils.countMatches(input, JOKER);
             return switch (jokers) {
-                case 5 -> FiveOfKind; // JJJJJ
-                case 4 -> FiveOfKind; // *JJJJ => QQQQQ
-                case 3 -> switch (distinctNumbers) { // **JJJ
-                    case 3 -> FourOfKind; // 23JJJ => 23333
-                    case 2 -> FiveOfKind; // 22JJJ => 22222
+                case 5 -> FiveOfKind; // 22222
+                case 4 -> FiveOfKind; // 2222* => 22222
+                case 3 -> switch (distinctNumbers) {
+                    case 3 -> FourOfKind; // 23*** => 23333
+                    case 2 -> FiveOfKind; // 22*** => 22222
                     default -> throw new IllegalStateException(input);
                 };
-                case 2 -> switch (distinctNumbers) { // ***JJ
-                    case 4 -> ThreeOfKind; // 234JJ => 23444
-                    case 3 -> FourOfKind; // 233JJ => 23333
-                    case 2 -> FiveOfKind; // 222JJ => 22222
+                case 2 -> switch (distinctNumbers) {
+                    case 4 -> ThreeOfKind; // 234** => 23444
+                    case 3 -> FourOfKind; // 233** => 23333
+                    case 2 -> FiveOfKind; // 222** => 22222
                     default -> throw new IllegalStateException(input);
                 };
-                case 1 -> switch (distinctNumbers) { // ****J
-                    case 5 -> OnePair; // 2345J => 23455
-                    case 4 -> ThreeOfKind; // 2234J => 22234
-                    case 3 -> longestRun == 3 ? FourOfKind : FullHouse; // 2233J => 22233 | 2223J => 22223
-                    case 2 -> FiveOfKind; // 2222J => 22222
+                case 1 -> switch (distinctNumbers) {
+                    case 5 -> OnePair; // 2345* => 23455
+                    case 4 -> ThreeOfKind; // 2234* => 22234
+                    case 3 -> longestRun == 3 ? FourOfKind : FullHouse; // 2233* => 22233 | 2223* => 22223
+                    case 2 -> FiveOfKind; // 2222* => 22222
                     default -> throw new IllegalStateException(input);
                 };
-                case 0 -> switch (distinctNumbers) { // *****
+                case 0 -> switch (distinctNumbers) {
                     case 5 -> HighCard; // 23456
                     case 4 -> OnePair; // 22345
                     case 3 -> longestRun == 3 ? ThreeOfKind : TwoPair; // 22334 | 22234
@@ -88,10 +76,11 @@ class Day7 {
     }
 
     record Card(Type type, String hand, long bid, String compareBy, long order) implements Comparable<Card> {
-        static Card from(final String input, final boolean allowJokers) {
+        static Card from(final String input, final boolean jokersAllowed) {
             final var raw = StringUtils.split(input);
-            final var order = StringUtils.replaceChars(raw[0], allowJokers ? "AKQT98765432J" : "AKQJT98765432", "MLKJIHGFEDCBA");
-            return new Card(Type.from(raw[0], allowJokers), raw[0], Long.parseLong(raw[1]), order, 0L);
+            final var hand = jokersAllowed ? StringUtils.replaceChars(raw[0], 'J', JOKER) : raw[0];
+            final var order = StringUtils.replaceChars(hand, "AKQJT98765432" + JOKER, "NMLKJIHGFEDCBA");
+            return new Card(Type.from(hand), hand, Long.parseLong(raw[1]), order, 0L);
         }
 
         Card changeOrder(final long order) {
