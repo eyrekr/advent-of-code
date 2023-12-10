0202c8454a26194cc4bb3bae4af294fdb57ecefe
Erik Kratochvil <erik.kratochvil@outlook.cz>
2023-12-10 16:57:49 +0100

    Day 11 Preparation

---
+++ 2023/src/main/java/com/github/eyrekr/D11.java
@@ -0,0 +1,24 @@
+package com.github.eyrekr;
+
+/**
+ * https://adventofcode.com/2023/day/10
+ * 1) 6903
+ * 2) 265
+ */
+class D11 extends AoC {
+
+    // parsed input
+
+    D11(final String input) {
+        super(input);
+        // parse input
+    }
+
+    long star1() {
+        return 0L;
+    }
+
+    long star2() {
+        return 0L;
+    }
+}
---
+++ 2023/src/test/java/com/github/eyrekr/TestD11.java
@@ -0,0 +1,31 @@
+package com.github.eyrekr;
+
+import com.github.eyrekr.util.Str;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TestD11 {
+    final String SAMPLE = """
+            """;
+
+    @Test
+    void sampleStar1() {
+        assertThat(new D11(SAMPLE).star1()).isEqualTo(0L);
+    }
+
+    @Test
+    void sampleStar2() {
+        assertThat(new D11(SAMPLE).star2()).isEqualTo(0L);
+    }
+
+    @Test
+    void star1() {
+        assertThat(new D11(Str.testResource("D11.txt")).star1()).isEqualTo(0L);
+    }
+
+    @Test
+    void star2() {
+        assertThat(new D11(Str.testResource("D11.txt")).star2()).isEqualTo(0L);
+    }
+}
