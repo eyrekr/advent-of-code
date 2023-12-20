34712865d666b79bbbfae867b83c7e5d053e36eb
Erik Kratochvil <erik.kratochvil@outlook.cz>
2023-12-20 19:18:42 +0100

    Refactoring

--- 2023/src/main/java/com/github/eyrekr/D20.java
+++ 2023/src/main/java/com/github/eyrekr/D20.java
@@ -22,77 +22,64 @@ class D20 extends AoC {
     static final String TERMINAL = "rx";
 
     final Seq<Module> modules;
-    final Map<String, Module> map;
+    final Map<String, Module> moduleByName;
     final Module broadcaster;
 
     D20(final String input) {
         super(input);
         modules = lines.mapWith(Seq.range(0, lines.length), Module::from);
-        map = modules.indexBy(module -> module.name);
-        broadcaster = map.get(BROADCASTER);
+        moduleByName = modules.indexBy(module -> module.name);
+        broadcaster = moduleByName.get(BROADCASTER);
 
         // add the "output" modules so that we do not have to constantly deal with NPE
         modules.each(module -> module.out
-                .where(out -> map.get(out) == null)
-                .each(name -> map.put(name, new Module(Type.Output, -1, name, Seq.empty()))));
-        // update activations of conjuctions modules (based on the number of inputs)
-        modules.each(module -> module.out.map(map::get)
+                .where(out -> moduleByName.get(out) == null)
+                .each(name -> moduleByName.put(name, new Module(Type.Output, -1, name, Seq.empty()))));
+        // update activations of CONJUNCTIONS modules based on the modules that feed them
+        modules.each(module -> module.out.map(moduleByName::get)
                 .where(Objects::nonNull)
                 .where(out -> out.type == Type.Conjunction)
                 .each(conjunction -> conjunction.activation |= 1L << module.id));
     }
 
     long star1() {
-        Pulses pulses = new Pulses(0, 0);
-        for (int i = 0; i < 1000; i++) pulses = pulses.plus(run());
-        return pulses.low * pulses.high;
-    }
-
-    Pulses run() {
         long numberOfLowPulses = 0, numerOfHighPulses = 0;
-        final LinkedList<Pulse> pulsesToProcess = new LinkedList<>();
-        pulsesToProcess.addFirst(new Pulse(null, broadcaster, 0));
 
-        final Module secondLast = modules.firstWhere(module -> module.out.has(TERMINAL));
-        Str.print("Second last = %s\n", secondLast);
+        for (int i = 0; i < 1000; i++) {
+            final LinkedList<Pulse> pulsesToProcess = new LinkedList<>();
+            pulsesToProcess.addFirst(new Pulse(null, broadcaster, 0));
 
+            while (!pulsesToProcess.isEmpty()) {
+                final Pulse pulse = pulsesToProcess.removeFirst();
+                final Module module = pulse.target;
+                final boolean low = pulse.value == 0;
+                final boolean high = !low;
 
-        while (!pulsesToProcess.isEmpty()) {
-            final Pulse pulse = pulsesToProcess.removeFirst();
-            final Module module = pulse.target;
-            final boolean low = pulse.value == 0;
-            final boolean high = !low;
+                if (low) numberOfLowPulses++;
+                if (high) numerOfHighPulses++;
 
-            if (low) numberOfLowPulses++;
-            if (high) numerOfHighPulses++;
+                final Consumer<Integer> emitPulse = value -> module.out
+                        .map(moduleByName::get)
+                        .map(nextTarget -> new Pulse(module, nextTarget, value))
+                        .each(pulsesToProcess::addLast);
 
-            final Consumer<Integer> emit = value -> module.out
-                    .map(map::get)
-                    .map(nextTarget -> new Pulse(module, nextTarget, value))
-                    .each(pulsesToProcess::addLast);
-
-            switch (module.type) { // SIGNAL LOGIC
-                case Broadcaster -> emit.accept(0);
-                case FlipFlop -> {
-                    if (low) {
-                        emit.accept(module.isActivated() ? 0 : 1);
-                        module.update(pulse);
+                switch (module.type) { // SIGNAL LOGIC
+                    case Broadcaster -> emitPulse.accept(0);
+                    case FlipFlop -> {
+                        if (low) {
+                            emitPulse.accept(module.isActivated() ? 0 : 1);
+                            module.update(pulse);
+                        }
                     }
-                }
-                case Conjunction -> {
-                    module.update(pulse);
-                    emit.accept(module.isActivated() ? 0 : 1);
+                    case Conjunction -> {
+                        module.update(pulse);
+                        emitPulse.accept(module.isActivated() ? 0 : 1);
+                    }
                 }
             }
         }
 
-        return new Pulses(numberOfLowPulses, numerOfHighPulses);
-    }
-
-    record Pulses(long low, long high) {
-        Pulses plus(final Pulses that) {
-            return new Pulses(low + that.low, high + that.high);
-        }
+        return numberOfLowPulses * numerOfHighPulses;
     }
 
 
@@ -126,7 +113,7 @@ class D20 extends AoC {
                 }
 
                 final Consumer<Integer> emit = value -> module.out
-                        .map(map::get)
+                        .map(moduleByName::get)
                         .map(nextTarget -> new Pulse(module, nextTarget, value))
                         .each(pulsesToProcess::addLast);
 
