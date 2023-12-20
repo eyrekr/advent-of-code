3d4b535cb58a108ae6fd62e08f3603510acfa054
Erik Kratochvil <erik.kratochvil@outlook.cz>
2023-12-20 07:08:34 +0100

    D20 Preparation

---
+++ 2023/src/main/java/com/github/eyrekr/D20.java
@@ -0,0 +1,21 @@
+package com.github.eyrekr;
+
+/**
+ * https://adventofcode.com/2023/day/20
+ * 1)
+ * 2)
+ */
+class D20 extends AoC {
+
+    D20(final String input) {
+        super(input);
+    }
+
+    long star1() {
+        return 0L;
+    }
+
+    long star2() {
+        return 0L;
+    }
+}
---
+++ 2023/src/test/java/com/github/eyrekr/TestD20.java
@@ -0,0 +1,49 @@
+package com.github.eyrekr;
+
+import com.github.eyrekr.util.Str;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TestD20 {
+    final String SAMPLE_A = """
+            broadcaster -> a, b, c
+            %a -> b
+            %b -> c
+            %c -> inv
+            &inv -> a
+            """;
+    final String SAMPLE_B = """
+            broadcaster -> a
+            %a -> inv, con
+            &inv -> b
+            %b -> con
+            &con -> output
+            """;
+
+    @Test
+    void sampleStar1A() {
+        assertThat(new D20(SAMPLE_A).star1()).isEqualTo(32_000_000L);
+    }
+
+    @Test
+    void sampleStar1B() {
+        assertThat(new D20(SAMPLE_B).star1()).isEqualTo(11_687_500L);
+    }
+
+    @Test
+    void sampleStar2() {
+        assertThat(new D20(SAMPLE_A).star2()).isEqualTo(0L);
+    }
+
+    @Test
+    void star1() {
+        assertThat(new D20(Str.testResource("D20.txt")).star1()).isEqualTo(0L);
+    }
+
+    @Test
+    void star2() {
+        assertThat(new D20(Str.testResource("D20.txt")).star2()).isEqualTo(0L);
+    }
+
+}
