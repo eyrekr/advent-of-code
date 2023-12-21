73182e840c29e125d9a95a1c1ae973c04a130149
Erik Kratochvil <erik.kratochvil@outlook.cz>
2023-12-21 09:03:23 +0100

    D21 Solution star 1

--- 2023/src/main/java/com/github/eyrekr/D21.java
+++ 2023/src/main/java/com/github/eyrekr/D21.java
@@ -1,23 +1,85 @@
 package com.github.eyrekr;
 
+import com.github.eyrekr.util.Grid;
+import com.github.eyrekr.util.Str;
+
 /**
  * https://adventofcode.com/2023/day/21
- * 1)
+ * 1) 3598
  * 2)
  */
 class D21 extends AoC {
 
+    static final int STONE = -9999;
+    static final int UNREACHABLE = -9;
+
+    final Grid grid;
+
     D21(final String input) {
         super(input);
+        grid = Grid.of(input);
     }
 
-    long star1() {
+    long star1(final int steps) {
+        // init
+        int[][] d = new int[grid.m][grid.n];
+        for (final var it : grid) {
+            switch (it.ch) {
+                case '#' -> d[it.x][it.y] = STONE;
+                case 'S' -> d[it.x][it.y] = 0;
+                default -> d[it.x][it.y] = UNREACHABLE;
+            }
+        }
+
+        // iterate
+        for (int step = 0; step < steps; step++) {
+            int[][] tmp = new int[grid.m][grid.n];
+            for (int y = 0; y < grid.n; y++) {
+                for (int x = 0; x < grid.m; x++) {
+                    if (d[x][y] == STONE) {
+                        tmp[x][y] = STONE;
+                    }
+                    if (d[x][y] == step) {
+                        if (x - 1 >= 0 && tmp[x - 1][y] != STONE) tmp[x - 1][y] = step + 1;
+                        if (x + 1 < grid.m && tmp[x + 1][y] != STONE) tmp[x + 1][y] = step + 1;
+                        if (y - 1 >= 0 && tmp[x][y - 1] != STONE) tmp[x][y - 1] = step + 1;
+                        if (y + 1 < grid.n && tmp[x][y + 1] != STONE) tmp[x][y + 1] = step + 1;
+                    }
+                }
+            }
+            d = tmp;
+
+            print(step, d);
+        }
+
+        // calculate the steps
+        long sum = 0;
+        for (int i = 0; i < grid.m * grid.n; i++)
+            if (d[i % grid.m][i / grid.m] == steps) sum++;
+        return sum;
+    }
+
+
+    long star2(final int steps) {
         return 0L;
     }
 
 
-    long star2() {
-        return 0L;
+    void print(final int step, final int[][] d) {
+        Str.print("After step @c%d\n", step + 1);
+        for (int y = 0; y < grid.n; y++) {
+            for (int x = 0; x < grid.m; x++) {
+                if (d[x][y] == STONE) {
+                    Str.print("@w#");
+                } else if (d[x][y] == step + 1) {
+                    Str.print("@rO");
+                } else {
+                    Str.print("@w.");
+                }
+            }
+            Str.print("\n");
+        }
+        Str.print("\n\n");
     }
 
 }
--- 2023/src/test/java/com/github/eyrekr/TestD21.java
+++ 2023/src/test/java/com/github/eyrekr/TestD21.java
@@ -7,26 +7,67 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class TestD21 {
     final String SAMPLE = """
+            ...........
+            .....###.#.
+            .###.##..#.
+            ..#.#...#..
+            ....#.#....
+            .##..S####.
+            .##..#...#.
+            .......##..
+            .##.#.####.
+            .##..##.##.
+            ...........
             """;
 
     @Test
     void sampleStar1() {
-        assertThat(new D21(SAMPLE).star1()).isEqualTo(0L);
+        assertThat(new D21(SAMPLE).star1(6)).isEqualTo(16L);
     }
 
     @Test
-    void sampleStar2() {
-        assertThat(new D21(SAMPLE).star2()).isEqualTo(0L);
+    void sampleStar2_After6Steps() {
+        assertThat(new D21(SAMPLE).star2(6)).isEqualTo(16L);
+    }
+
+    @Test
+    void sampleStar2_After16Steps() {
+        assertThat(new D21(SAMPLE).star2(16)).isEqualTo(50L);
+    }
+
+    @Test
+    void sampleStar2_After50Steps() {
+        assertThat(new D21(SAMPLE).star2(50)).isEqualTo(1594L);
+    }
+
+    @Test
+    void sampleStar2_After100Steps() {
+        assertThat(new D21(SAMPLE).star2(100)).isEqualTo(6536L);
+    }
+
+    @Test
+    void sampleStar2_After500Steps() {
+        assertThat(new D21(SAMPLE).star2(500)).isEqualTo(167004L);
+    }
+
+    @Test
+    void sampleStar2_After1000Steps() {
+        assertThat(new D21(SAMPLE).star2(1000)).isEqualTo(668697L);
+    }
+
+    @Test
+    void sampleStar2_After5000Steps() {
+        assertThat(new D21(SAMPLE).star2(5000)).isEqualTo(16733044L);
     }
 
     @Test
     void star1() {
-        assertThat(new D21(Str.testResource("D21.txt")).star1()).isEqualTo(0L);
+        assertThat(new D21(Str.testResource("D21.txt")).star1(64)).isEqualTo(3598L);
     }
 
     @Test
     void star2() {
-        assertThat(new D21(Str.testResource("D21.txt")).star2()).isEqualTo(0L);
+        assertThat(new D21(Str.testResource("D21.txt")).star2(26501365)).isEqualTo(0L);
     }
 
 }
