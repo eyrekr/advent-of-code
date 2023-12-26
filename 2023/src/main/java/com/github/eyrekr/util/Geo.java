3281c72739f35bc6b648106a43bbc652fb5d8c7f
Erik Kratochvil <erik.kratochvil@outlook.cz>
2023-12-26 08:18:27 +0100

    Geo refactoring

--- 2023/src/main/java/com/github/eyrekr/util/Geo.java
+++ 2023/src/main/java/com/github/eyrekr/util/Geo.java
@@ -6,43 +6,6 @@ package com.github.eyrekr.util;
 public final class Geo {
 
     /**
-     * Using Bresenham's algorithm to calculate the shortest distance between two points.
-     * https://www.baeldung.com/cs/bresenhams-line-algorithm
-     *
-     * @param a point A
-     * @param b point B
-     * @return Distance between A and B
-     */
-    public static long distance(final P a, final P b) {
-        long steps = 0L;
-        long x1 = b.x, x0 = a.x, y1 = b.y, y0 = a.y;
-
-        final long dx = Math.abs(x1 - x0);
-        final long sx = x0 < x1 ? 1 : -1;
-        final long dy = -Math.abs(y1 - y0);
-        final long sy = y0 < y1 ? 1 : -1;
-        long error = dx + dy;
-
-        while (true) {
-            steps++;//plot
-
-            if (x0 == x1 && y0 == y1) break;
-            final long e2 = 2 * error;
-            if (e2 >= dy) {
-                if (x0 == x1) break;
-                error = error + dy;
-                x0 = x0 + sx;
-            }
-            if (e2 <= dx) {
-                if (y0 == y1) break;
-                error = error + dx;
-                y0 = y0 + sy;
-            }
-        }
-        return steps;
-    }
-
-    /**
      * Point in 2D space.
      */
     public static final class P {
@@ -59,15 +22,45 @@ public final class Geo {
         }
 
         public P translate(final long dx, final long dy) {
-            return new P(x+dx, y+dy);
+            return new P(x + dx, y + dy);
         }
 
-        public long distance(final P that) {
-            return Geo.distance(this, that);
+        /**
+         * Using Bresenham's algorithm to calculate the shortest distance between two points.
+         * https://www.baeldung.com/cs/bresenhams-line-algorithm
+         */
+        public long bresenhamDistance(final P that) {
+            long steps = 0L;
+            long x1 = that.x, x0 = this.x, y1 = that.y, y0 = this.y;
+
+            final long dx = Math.abs(x1 - x0);
+            final long sx = x0 < x1 ? 1 : -1;
+            final long dy = -Math.abs(y1 - y0);
+            final long sy = y0 < y1 ? 1 : -1;
+            long error = dx + dy;
+
+            while (true) {
+                steps++;//plot
+
+                if (x0 == x1 && y0 == y1) break;
+                final long e2 = 2 * error;
+                if (e2 >= dy) {
+                    if (x0 == x1) break;
+                    error = error + dy;
+                    x0 = x0 + sx;
+                }
+                if (e2 <= dx) {
+                    if (y0 == y1) break;
+                    error = error + dx;
+                    y0 = y0 + sy;
+                }
+            }
+            return steps;
         }
 
         public long manhattanDistance(final P that) {
             return Math.abs(this.x - that.x) + Math.abs(this.y - that.y);
         }
     }
+
 }
