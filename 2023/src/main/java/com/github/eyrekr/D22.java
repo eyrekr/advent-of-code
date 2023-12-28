9669e4676e6eebf6e916859083be319ddf191743
Erik Kratochvil <erik.kratochvil@team.wrike.com>
2023-12-28 10:12:03 +0100

    D22

---
+++ 2023/src/main/java/com/github/eyrekr/D22.java
@@ -0,0 +1,24 @@
+package com.github.eyrekr;
+
+/**
+ * https://adventofcode.com/2023/day/22
+ * 1)
+ * 2)
+ */
+class D22 extends AoC {
+
+    D22(final String input) {
+        super(input);
+    }
+
+    @Override
+    long star1() {
+        return 0L;
+    }
+
+
+    @Override
+    long star2() {
+        return 0L;
+    }
+}
---
+++ 2023/src/test/java/com/github/eyrekr/D22Test.java
@@ -0,0 +1,40 @@
+package com.github.eyrekr;
+
+import com.github.eyrekr.util.Str;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class D22Test {
+    final String sample = """
+            1,0,1~1,2,1
+            0,0,2~2,0,2
+            0,2,3~2,2,3
+            0,0,4~0,2,4
+            2,0,5~2,2,5
+            0,1,6~2,1,6
+            1,1,8~1,1,9
+            """;
+    final String input = Str.testResource("D22.txt");
+
+    @Test
+    void sampleStar1() {
+        assertThat(new D22(sample).star1()).isEqualTo(0L);
+    }
+
+    @Test
+    void sampleStar2() {
+        assertThat(new D22(sample).star2()).isEqualTo(0L);
+    }
+
+    @Test
+    void star1() {
+        assertThat(new D22(input).star1()).isEqualTo(0L);
+    }
+
+    @Test
+    void star2() {
+        assertThat(new D22(input).star2()).isEqualTo(0L);
+    }
+
+}
