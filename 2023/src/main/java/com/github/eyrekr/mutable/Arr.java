c5068fe17909e5943133e2d5cd9c95e718842a2f
Erik Kratochvil <erik.kratochvil@outlook.cz>
2024-01-02 08:02:38 +0100

    Arr implementation without delegate

--- 2023/src/main/java/com/github/eyrekr/mutable/Arr.java
+++ 2023/src/main/java/com/github/eyrekr/mutable/Arr.java
@@ -235,12 +235,15 @@ public final class Arr<E> implements Iterable<E> {
     }
 
     public Arr<E> where(final Predicate<? super E> predicate) {
-
+        int n = 0;
+        for (int i = 0; i < length; i++)
+            if (predicate.test(at(i))) swap(i, n++);
+        length = n;
         return this;
     }
 
     public Arr<E> reversed() {
-        for (int i = 0; i < length / 2; i++) swap(i, length - i); // FIXME Reverse in O(1)
+        for (int i = 0; i < length / 2; i++) swap(i, length - i); // FIXME Implement reverse in O(1)
         return this;
     }
 
