91c978ae5fcdfc5547f77a557ef838a6a9f86f13
Erik Kratochvil <erik.kratochvil@team.wrike.com>
2024-01-08 20:48:14 +0100

    D6 Star 1

--- java/src/main/java/com/github/eyrekr/y2022/D06.java
+++ java/src/main/java/com/github/eyrekr/y2022/D06.java
@@ -2,11 +2,23 @@ package com.github.eyrekr.y2022;
 
 public class D06 {
 
+    final String input;
+
     D06(final String input) {
+        this.input = input;
     }
 
     long star1() {
-        return 0L;
+        char a1 = 0, a2 = 0, a3 = 0, a4 = 0;
+        for (int i = 0; i < input.length(); i++) {
+            final boolean allDifferent = a1 != a2 && a1 != a3 && a1 != a4 && a2 != a3 && a2 != a4 && a3 != a4;
+            if (i >= 4 && allDifferent) return i;
+            a1 = a2;
+            a2 = a3;
+            a3 = a4;
+            a4 = input.charAt(i);
+        }
+        return -1;
     }
 
     long star2() {
--- java/src/test/java/com/github/eyrekr/y2022/D06Test.java
+++ java/src/test/java/com/github/eyrekr/y2022/D06Test.java
@@ -3,17 +3,29 @@ package com.github.eyrekr.y2022;
 import com.github.eyrekr.output.Out;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 class D06Test {
 
-    final String sample = """
-            """;
-
     final String input = Out.testResource("2022/D06.txt");
 
-    @Test
-    void sampleStar1() {
-        Assertions.assertThat(new D06(sample).star1()).isEqualTo(0L);
+    @ParameterizedTest
+    @MethodSource("dataForSampleStar1")
+    void sampleStar1(final String line, final long expected) {
+        Assertions.assertThat(new D06(line).star1()).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> dataForSampleStar1() {
+        return Stream.of(
+                Arguments.of("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7L),
+                Arguments.of("bvwbjplbgvbhsrlpgdmjqwftvncz", 5L),
+                Arguments.of("nppdvjthqldpwncqszvftbrmjlhg", 6L),
+                Arguments.of("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10L),
+                Arguments.of("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11L));
     }
 
     @Test
@@ -21,10 +33,10 @@ class D06Test {
         Assertions.assertThat(new D06(input).star1()).isEqualTo(0L);
     }
 
-    @Test
-    void sampleStar2() {
-        Assertions.assertThat(new D06(sample).star2()).isEqualTo(0L);
-    }
+//    @Test
+//    void sampleStar2() {
+//        Assertions.assertThat(new D06(sample).star2()).isEqualTo(0L);
+//    }
 
     @Test
     void star2() {
