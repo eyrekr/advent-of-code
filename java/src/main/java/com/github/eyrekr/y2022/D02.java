a92b332d3f7d746f52a794a72c86d9fb67e2760f
Erik Kratochvil <erik.kratochvil@team.wrike.com>
2024-01-08 14:37:09 +0100

    D2 Solution

--- java/src/main/java/com/github/eyrekr/y2022/D02.java
+++ java/src/main/java/com/github/eyrekr/y2022/D02.java
@@ -15,38 +15,33 @@ public class D02 {
 
     long star1() {
         // A|X=rock  B|Y=paper  C|Z=scissors
-        final Map<String, Round> rounds = ImmutableMap.<String, Round>builder()
-                .put("A X", new Round(Hand.Rock, Hand.Rock, 1 + 3))
-                .put("A Y", new Round(Hand.Rock, Hand.Paper, 2 + 6))
-                .put("A Z", new Round(Hand.Rock, Hand.Scissors, 3 + 0))
-                .put("B X", new Round(Hand.Paper, Hand.Rock, 1 + 0))
-                .put("B Y", new Round(Hand.Paper, Hand.Paper, 2 + 3))
-                .put("B Z", new Round(Hand.Paper, Hand.Scissors, 3 + 6))
-                .put("C X", new Round(Hand.Scissors, Hand.Rock, 1 + 6))
-                .put("C Y", new Round(Hand.Scissors, Hand.Paper, 2 + 0))
-                .put("C Z", new Round(Hand.Scissors, Hand.Scissors, 3 + 3))
+        final Map<String, Integer> score = ImmutableMap.<String, Integer>builder()
+                .put("A X", 1 + 3)
+                .put("A Y", 2 + 6)
+                .put("A Z", 3 + 0)
+                .put("B X", 1 + 0)
+                .put("B Y", 2 + 3)
+                .put("B Z", 3 + 6)
+                .put("C X", 1 + 6)
+                .put("C Y", 2 + 0)
+                .put("C Z", 3 + 3)
                 .build();
-        return lines.map(rounds::get).toLongs(Round::score).sum();
+        return lines.toLongs(score::get).sum();
     }
 
     long star2() {
         // X=lose  Y=draw  Z=win
-        final Map<String, Round> rounds = ImmutableMap.<String, Round>builder()
-                .put("A X", new Round(Hand.Rock, Hand.Scissors, 3 + 0))
-                .put("A Y", new Round(Hand.Rock, Hand.Rock, 1 + 3))
-                .put("A Z", new Round(Hand.Rock, Hand.Paper, 2 + 6))
-                .put("B X", new Round(Hand.Paper, Hand.Rock, 1 + 0))
-                .put("B Y", new Round(Hand.Paper, Hand.Paper, 2 + 3))
-                .put("B Z", new Round(Hand.Paper, Hand.Scissors, 3 + 6))
-                .put("C X", new Round(Hand.Scissors, Hand.Paper, 2 + 0))
-                .put("C Y", new Round(Hand.Scissors, Hand.Scissors, 3 + 3))
-                .put("C Z", new Round(Hand.Scissors, Hand.Rock, 1 + 6))
+        final Map<String, Integer> score = ImmutableMap.<String, Integer>builder()
+                .put("A X", 3 + 0)
+                .put("A Y", 1 + 3)
+                .put("A Z", 2 + 6)
+                .put("B X", 1 + 0)
+                .put("B Y", 2 + 3)
+                .put("B Z", 3 + 6)
+                .put("C X", 2 + 0)
+                .put("C Y", 3 + 3)
+                .put("C Z", 1 + 6)
                 .build();
-        return lines.map(rounds::get).toLongs(Round::score).sum();
+        return lines.toLongs(score::get).sum();
     }
-
-    record Round(Hand his, Hand yours, int score) {
-    }
-
-    enum Hand {Rock, Paper, Scissors}
 }
