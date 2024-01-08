0b153f5b4d13ba4112d5c4fd83765eda218bc2b0
Erik Kratochvil <erik.kratochvil@team.wrike.com>
2024-01-08 14:37:56 +0100

    D3 Template

---
+++ java/src/test/java/com/github/eyrekr/y2022/D03Test.java
@@ -0,0 +1,32 @@
+package com.github.eyrekr.y2022;
+
+import com.github.eyrekr.output.Out;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class D03Test {
+
+    final String sample = """
+            """;
+    final String input = Out.testResource("2022/D03.txt");
+
+    @Test
+    void sampleStar1() {
+        Assertions.assertThat(new D03(sample).star1()).isEqualTo(0L);
+    }
+
+    @Test
+    void sampleStar2() {
+        Assertions.assertThat(new D03(sample).star2()).isEqualTo(0L);
+    }
+
+    @Test
+    void star1() {
+        Assertions.assertThat(new D03(input).star1()).isEqualTo(0L);
+    }
+
+    @Test
+    void star2() {
+        Assertions.assertThat(new D03(input).star2()).isEqualTo(0L);
+    }
+}
