79faf9ac27ce00839856bf9befe0546fea1a9d05
Erik Kratochvil <erik.kratochvil@team.wrike.com>
2024-01-08 16:02:24 +0100

    Refactoring

--- java/src/main/java/com/github/eyrekr/y2022/D03.java
+++ java/src/main/java/com/github/eyrekr/y2022/D03.java
@@ -15,36 +15,36 @@ public class D03 {
     long star1() {
         return rucksacks.map(contents -> {
                     final String left = contents.substring(0, contents.length() / 2), right = contents.substring(contents.length() / 2);
-                    return new Histogram().and(left).and(right);
+                    return new Indicator().and(left).and(right);
                 })
-                .toLongs(Histogram::topPriority)
+                .toLongs(Indicator::topPriority)
                 .sum();
     }
 
     long star2() {
         return rucksacks.batch(3)
-                .map(group -> group.reduce(new Histogram(), Histogram::and))
-                .toLongs(Histogram::topPriority)
+                .map(group -> group.reduce(new Indicator(), Indicator::and))
+                .toLongs(Indicator::topPriority)
                 .sum();
     }
 
-    static class Histogram {
-        final boolean[] h = new boolean[64];
+    static class Indicator {
+        final boolean[] ch = new boolean[64];
 
         {
-            Arrays.fill(h, true);
+            Arrays.fill(ch, true);
         }
 
-        Histogram and(final String input) {
+        Indicator and(final String input) {
             final boolean[] t = new boolean[64];
             for (int i = 0; i < input.length(); i++) t[priority(input.charAt(i))] = true;
-            for (int i = 0; i < 64; i++) h[i] = h[i] && t[i];
+            for (int i = 0; i < 64; i++) ch[i] = ch[i] && t[i];
             return this;
         }
 
         int topPriority() {
-            for (int i = 0; i < h.length; i++)
-                if (h[i]) return i;
+            for (int i = 0; i < ch.length; i++)
+                if (ch[i]) return i;
             throw new IllegalStateException();
         }
 
