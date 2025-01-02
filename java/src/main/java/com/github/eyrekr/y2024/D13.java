c70813622b62789242cedab2e166fdf3ef9bcba6
eyrekr <erik.kratochvil@outlook.cz>
2025-01-02 15:15:55 +0100

    refactoring

--- java/src/main/java/com/github/eyrekr/y2024/D13.java
+++ java/src/main/java/com/github/eyrekr/y2024/D13.java
@@ -2,11 +2,13 @@ package com.github.eyrekr.y2024;
 
 import com.github.eyrekr.Aoc;
 import com.github.eyrekr.immutable.Arr;
+import com.github.eyrekr.immutable.Int;
 import com.github.eyrekr.immutable.Longs;
 import org.apache.commons.lang3.StringUtils;
 
 class D13 extends Aoc {
 
+    static final Int domain = new Int(0, 100);
     final Arr<ClawMachine> clawMachines;
 
     D13(final String input) {
@@ -17,7 +19,7 @@ class D13 extends Aoc {
     @Override
     public long star1() {
         return clawMachines
-                .mapToLongs(machine -> machine.price(100))
+                .mapToLongs(ClawMachine::price)
                 .where(price -> price > 0)
                 .sum();
     }
@@ -28,13 +30,13 @@ class D13 extends Aoc {
             return new ClawMachine(l.at(0), l.at(1), l.at(2), l.at(3), l.at(4), l.at(5));
         }
 
-        long price(final long limit) {
+        long price() {
             final long d = ax * by - ay * bx;
             if (d == 0)
                 throw new IllegalStateException(String.format("A[%d,%d]  B=[%d,%d] are colinear", ax, ay, bx, by));
             final long b = (y * ax - x * ay) / d, a = (x - b * bx) / ax;
             if (a * ax + b * bx != x || a * ay + b * by != y) return -1; // solution does not exist
-            return 0 <= a && a < limit && 0 <= b && b < limit ? 3 * a + b : -1;
+            return domain.contains(a) && domain.contains(b) ? 3 * a + b : -1;
         }
     }
 
