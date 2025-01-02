863769692bab9ccd137501fa2db17b0df3351edc
eyrekr <erik.kratochvil@outlook.cz>
2025-01-02 15:11:41 +0100

    2024, day 13, star 1

---
+++ java/src/main/java/com/github/eyrekr/y2024/D13.java
@@ -0,0 +1,41 @@
+package com.github.eyrekr.y2024;
+
+import com.github.eyrekr.Aoc;
+import com.github.eyrekr.immutable.Arr;
+import com.github.eyrekr.immutable.Longs;
+import org.apache.commons.lang3.StringUtils;
+
+class D13 extends Aoc {
+
+    final Arr<ClawMachine> clawMachines;
+
+    D13(final String input) {
+        final var definitions = StringUtils.splitByWholeSeparator(input, "\n\n");
+        clawMachines = Arr.fromArray(definitions).map(ClawMachine::fromString);
+    }
+
+    @Override
+    public long star1() {
+        return clawMachines
+                .mapToLongs(machine -> machine.price(100))
+                .where(price -> price > 0)
+                .sum();
+    }
+
+    record ClawMachine(long ax, long ay, long bx, long by, long x, long y) {
+        static ClawMachine fromString(final String definition) {
+            final var l = Longs.fromString(definition);
+            return new ClawMachine(l.at(0), l.at(1), l.at(2), l.at(3), l.at(4), l.at(5));
+        }
+
+        long price(final long limit) {
+            final long d = ax * by - ay * bx;
+            if (d == 0)
+                throw new IllegalStateException(String.format("A[%d,%d]  B=[%d,%d] are colinear", ax, ay, bx, by));
+            final long b = (y * ax - x * ay) / d, a = (x - b * bx) / ax;
+            if (a * ax + b * bx != x || a * ay + b * by != y) return -1; // solution does not exist
+            return a < limit && b < limit ? 3 * a + b : -1;
+        }
+    }
+
+}
