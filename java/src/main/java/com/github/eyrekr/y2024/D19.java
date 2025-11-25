8620407750b107bb038c739bf03a4f614c5e783c
Erik Kratochvil <erik.kratochvil@team.wrike.com>
2025-11-25 20:37:45 +0100

    implement using cache explicitly

--- java/src/main/java/com/github/eyrekr/y2024/D19.java
+++ java/src/main/java/com/github/eyrekr/y2024/D19.java
@@ -1,47 +1,50 @@
 package com.github.eyrekr.y2024;
 
 import com.github.eyrekr.Aoc;
-import com.github.eyrekr.common.MemoizedFunction;
 import com.github.eyrekr.mutable.Arr;
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 class D19 extends Aoc {
 
     final Arr<String> towels;
     final Arr<String> designs;
-    final MemoizedFunction<String, Long> waysToComposeDesign;
+    private final Map<String, Long> memory = new HashMap<>();
 
     D19(final String input) {
         final var blocks = StringUtils.splitByWholeSeparator(input, "\n\n");
         towels = Arr.ofWordsFromString(blocks[0]);
         designs = Arr.ofLinesFromString(blocks[1]);
-
-        waysToComposeDesign = new MemoizedFunction<>() {
-            @Override
-            protected Long calculate(String design) {
-                return towels
-                        .map(towel -> {
-                            if (Objects.equals(design, towel)) return 1L;
-                            if (design.startsWith(towel)) return get(design.substring(towel.length()));
-                            return 0L;
-                        })
-                        .reduce(Long::sum)
-                        .getOrElse(0L);
-            }
-        };
     }
 
     @Override
     public long star1() {
-        return designs.map(waysToComposeDesign::get).countWhere(value -> value > 0);
+        return designs.map(this::calculateArrangementCount).countWhere(value -> value > 0);
     }
 
 
     @Override
     public long star2() {
-        return designs.map(waysToComposeDesign::get).reduce(Long::sum).getOrElse(-1L);
+        return designs.map(this::calculateArrangementCount).reduce(Long::sum).getOrElse(-1L);
+    }
+
+    long calculateArrangementCount(final String design) {
+        if (memory.containsKey(design)) return memory.get(design);
+
+        final long arrangementCount = towels
+                .map(towel -> {
+                    if (Objects.equals(design, towel)) return 1L;
+                    if (design.startsWith(towel)) return calculateArrangementCount(design.substring(towel.length()));
+                    return 0L;
+                })
+                .reduce(Long::sum)
+                .getOrElse(0L);
+
+        memory.put(design, arrangementCount);
+        return arrangementCount;
     }
 
 }
