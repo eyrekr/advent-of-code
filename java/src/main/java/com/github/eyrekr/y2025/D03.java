81882dde02b27f2e3bfc5b3a77ef98d0f21f6712
Erik Kratochvil <erik.kratochvil@team.wrike.com>
2025-12-29 16:08:21 +0100

    D3 template

---
+++ java/src/main/java/com/github/eyrekr/y2025/D03.java
@@ -0,0 +1,24 @@
+package com.github.eyrekr.y2025;
+
+import com.github.eyrekr.Aoc;
+import com.github.eyrekr.immutable.Seq;
+
+class D03 extends Aoc {
+
+    final Seq<String> banks;
+
+    D03(final String input) {
+        banks = Seq.ofLinesFromString(input);
+    }
+
+    @Override
+    public long star1() {
+        return -1L;
+    }
+
+    @Override
+    public long star2() {
+        return -1L;
+    }
+
+}
---
+++ java/src/test/java/com/github/eyrekr/y2025/D03Test.java
@@ -0,0 +1,18 @@
+package com.github.eyrekr.y2025;
+
+import com.github.eyrekr.AocTest;
+
+class D03Test extends AocTest {
+
+    D03Test() {
+        constructor(D03::new);
+        sample("""
+                987654321111111
+                811111111111119
+                234234234234278
+                818181911112111
+                """);
+        star1(357L, -1L);
+        star2(-1L, -1L);
+    }
+}
