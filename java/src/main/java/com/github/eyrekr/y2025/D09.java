4e89b11db213e97fff0d0aee49abb3dcb8f19982
Erik Kratochvil <erik.kratochvil@team.wrike.com>
2025-12-30 17:07:38 +0100

    d9 star 1

--- java/src/main/java/com/github/eyrekr/y2025/D09.java
+++ java/src/main/java/com/github/eyrekr/y2025/D09.java
@@ -9,12 +9,12 @@ class D09 extends Aoc {
     final Arr<P> redTiles;
 
     D09(final String input) {
-        redTiles = Arr.ofLinesFromString(input).map(P::new);
+        redTiles = Arr.ofLinesFromString(input).map(P::of);//.sortedBy(P::x);
     }
 
     @Override
     public long star1() {
-        return -1L;
+        return redTiles.prodUpperTriangleWith(redTiles, P::area).max(Long::compareTo).get();
     }
 
     @Override
@@ -22,18 +22,16 @@ class D09 extends Aoc {
         return -1L;
     }
 
-    static final class P {
-        final long x, y;
-
-        P(final String line) {
+    record P(long x, long y) {
+        static P of(final String line) {
             final String[] p = StringUtils.split(line, ',');
-            this.x = Long.parseLong(p[0]);
-            this.y = Long.parseLong(p[1]);
+            return new P(Long.parseLong(p[0]), Long.parseLong(p[1]));
         }
 
-        @Override
-        public String toString() {
-            return x + "," + y;
+        long area(final P that) {
+            final long width = Math.abs(this.x - that.x) + 1;
+            final long height = Math.abs(this.y - that.y) + 1;
+            return width * height;
         }
     }
 }
