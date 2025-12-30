fcf365c5fd9ad5f90c215a7fef9a9f8ff21dd973
Erik Kratochvil <erik.kratochvil@team.wrike.com>
2025-12-30 09:48:54 +0100

    day 6, star 2

--- java/src/main/java/com/github/eyrekr/y2025/D06.java
+++ java/src/main/java/com/github/eyrekr/y2025/D06.java
@@ -8,19 +8,17 @@ import org.apache.commons.lang3.StringUtils;
 class D06 extends Aoc {
 
     final Arr<String> lines;
-    final Arr<Operation> operations;
-    final Arr<Longs> rows;
 
     D06(final String input) {
         lines = Arr.ofLinesFromString(input);
-        rows = lines.removeLast().map(Longs::fromString);
-
-        final String lastLine = StringUtils.remove(lines.at(-1), ' ');
-        operations = Arr.ofCharactersFromString(lastLine).map(Operation::fromSymbol);
     }
 
     @Override
     public long star1() {
+        final String lastLineWithOperatorsOnly = StringUtils.remove(lines.at(-1), ' ');
+        final Arr<Operation> operations = Arr.ofCharactersFromString(lastLineWithOperatorsOnly).map(Operation::fromSymbol);
+        final Arr<Longs> rows = lines.removeLast().map(Longs::fromString);
+
         return Longs.range(0, operations.length)
                 .map(i -> rows.mapToLongs(row -> row.at(i)).reduce(operations.at(i)))
                 .sum();
@@ -33,18 +31,18 @@ class D06 extends Aoc {
         final Longs numbers = numbersByColumns(lines.removeLast());
         return positions.mapWith(
                         positions.removeFirst().addLast(lastLine.length()),
-                        (i, j) -> numbers.between(i, j - 2).reduce(operations.at(i)))
+                        (i, j) -> numbers.between(i, j - 2).reduce(Operation.fromSymbol(lastLine.charAt(i))))
                 .reduce(Long::sum)
                 .get();
     }
 
     Longs numbersByColumns(final Arr<String> lines) {
-        final int n = lines.at(0).length();
+        final int n = lines.map(String::length).max(Integer::compare).get();
         return Longs.range(0, n)
                 .map(column -> lines.reduce(
                         0L,
                         (value, line) -> {
-                            final char ch = line.charAt((int) column);
+                            final char ch = column < line.length() ? line.charAt((int) column) : ' ';
                             return Character.isDigit(ch) ? value * 10 + Character.digit(ch, 10) : value;
                         }));
     }
