0b5c64780a0403ebacbd2527b81a8c21256ee358
Erik Kratochvil <erik.kratochvil@team.wrike.com>
2026-01-05 20:23:00 +0100

    d9s2

--- java/src/main/java/com/github/eyrekr/y2025/D09.java
+++ java/src/main/java/com/github/eyrekr/y2025/D09.java
@@ -18,12 +18,39 @@ class D09 extends Aoc {
     }
 
     @Override
-    public long star2() {
+    public long star2() { //4753531494 too high
         return redTiles
                 .prodUpperTriangleWith(redTiles, Rectangle::new)
-                .where(rectangle -> rectangle.inside(redTiles))
+                .where(this::isRectangleInsidePolygon)
                 .mapToLongs(Rectangle::area)
                 .max();
+    }
+
+    boolean isRectangleInsidePolygon(final Rectangle rectangle) {
+        return isPointInsidePolygon(new P(rectangle.x1, rectangle.y1))
+                && isPointInsidePolygon(new P(rectangle.x1, rectangle.y2))
+                && isPointInsidePolygon(new P(rectangle.x2, rectangle.y1))
+                && isPointInsidePolygon(new P(rectangle.x2, rectangle.y2));
+    }
+
+    boolean isPointInsidePolygon(final P p) {
+        int cuts = 0;
+        for (int i = 0; i < redTiles.length; i++) {
+            final P a = redTiles.at(i), b = redTiles.at(i + 1);
+            if (isPointOnLine(p, a, b)) return true;
+            if (isRayCuttingLine(p, a, b)) cuts++;
+        }
+        return cuts % 2 == 1;
+    }
+
+    boolean isPointOnLine(final P p, final P l1, final P l2) {
+        if (p.equals(l1) || p.equals(l2)) return true;
+        return p.y == l1.y && p.y == l2.y && Math.min(l1.x, l2.x) <= p.x && p.x <= Math.max(l1.x, l2.x)
+                || p.x == l1.x && p.x == l2.x && Math.min(l1.y, l2.y) <= p.y && p.y <= Math.max(l1.y, l2.y);
+    }
+
+    boolean isRayCuttingLine(final P p, final P l1, final P l2) {
+        return l1.y != l2.y && l1.x == l2.x && p.x <= l1.x && Math.min(l1.y, l2.y) <= p.y && p.y <= Math.max(l1.y, l2.y);
     }
 
     record P(long x, long y) {
@@ -43,20 +70,26 @@ class D09 extends Aoc {
         }
 
         boolean inside(final Arr<P> polygon) {
-            final boolean atLeastOnePointOfPolygonIsInsideRectangle = polygon.atLeastOneIs(this::inside);
+            final boolean atLeastOnePointOfPolygonIsInsideRectangle = polygon.atLeastOneIs(this::isThePointInside);
             if (atLeastOnePointOfPolygonIsInsideRectangle) return false;
 
+            //at least one edge of the polygon cuts through the rectangle
+            for (int i = 0; i <= polygon.length; i++)
+                if (cuts(polygon.at(i), polygon.at(i + 1))) return false;
 
 
-//            for (int i = 0; i <= polygon.length; i++) {
-//                final P p1 = polygon.at(i), p2 = polygon.at(i + 1);
-//                if(x1 < p1.x && p1.x < x2 && x1 < p2.x && p2.x < x2)
-//            }
             return true;
         }
 
-        boolean inside(final P p) {
+        boolean isThePointInside(final P p) {
             return x1 < p.x && p.x < x2 && y1 < p.y && p.y < y2;
+        }
+
+        boolean cuts(final P p1, final P p2) {
+            final boolean horizontal = p1.y == p2.y;
+            return horizontal
+                    ? Math.min(p1.x, p2.x) <= x1 && x2 <= Math.max(p1.x, p2.x)
+                    : Math.min(p1.y, p2.y) <= y1 && y2 <= Math.max(p1.y, p2.y);
         }
     }
 }
